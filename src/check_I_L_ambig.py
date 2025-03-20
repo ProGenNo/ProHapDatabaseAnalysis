e7860695722ef1b10e495ffb63cf60c0bd502a1d
Jakub Vasicek <j.vasicek58@gmail.com>
2025-03-20 12:46:35 +0100

    check for I/L ambiguities in peptides

--- src/check_I_L_ambig.py
+++ src/check_I_L_ambig.py
@@ -27,7 +27,7 @@ prohap_il_peptides = pep_df[pep_df['Sequence'].str.contains('I') | pep_df['Seque
 prohap_il_peptides['ID'] = prohap_il_peptides['ID'].apply(lambda x: 'prohap_'+x)
 prohap_il_peptides['Database'] = 'ProHap_1kGP_ALL'
 prohap_il_peptides['Sequence_I_to_L'] = prohap_il_peptides['Sequence'].apply(lambda seq: seq.replace('I', 'L'))
-prohap_il_peptides['Ambiguous'] = prohap_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], prohap_il_peptides))
+prohap_il_peptides['Ambiguous'] = prohap_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], prohap_il_peptides), axis=1)
 
 print ('Reading', args.input_file_sp)
 sp_pep_df = pd.read_table(args.input_file_sp)
@@ -35,7 +35,7 @@ sp_il_peptides = sp_pep_df[sp_pep_df['Sequence'].str.contains('I') | sp_pep_df['
 sp_il_peptides['ID'] = sp_il_peptides['ID'].apply(lambda x: 'swissprot_'+x)
 sp_il_peptides['Database'] = 'SwissProt'
 sp_il_peptides['Sequence_I_to_L'] = sp_il_peptides['Sequence'].apply(lambda seq: seq.replace('I', 'L'))
-sp_il_peptides['Ambiguous'] = sp_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], sp_il_peptides))
+sp_il_peptides['Ambiguous'] = sp_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], sp_il_peptides), axis=1)
 
 print ('Reading', args.input_file_iso)
 iso_pep_df = pd.read_table(args.input_file_iso)
@@ -43,6 +43,6 @@ iso_il_peptides = iso_pep_df[iso_pep_df['Sequence'].str.contains('I') | iso_pep_
 iso_il_peptides['ID'] = iso_il_peptides['ID'].apply(lambda x: 'uniprot_'+x)
 iso_il_peptides['Database'] = 'UniProt_isoform'
 iso_il_peptides['Sequence_I_to_L'] = iso_il_peptides['Sequence'].apply(lambda seq: seq.replace('I', 'L'))
-iso_il_peptides['Ambiguous'] = iso_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], iso_il_peptides))
+iso_il_peptides['Ambiguous'] = iso_il_peptides.apply(lambda row: get_conflicting_peptides(row['Sequence_I_to_L'], row['Sequence'], iso_il_peptides), axis=1)
 
 pd.concat([prohap_il_peptides, sp_il_peptides, iso_il_peptides]).to_csv(args.output_file, header=True, index=False)
